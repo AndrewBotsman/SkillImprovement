3cfe66120b3635e6c6d0d463db22d5cec1a6e866
Andrew Botsman <Andrew_Botsman@mail.ru>
2018-12-19 01:38:00 +0200

    Initial commit

---
+++ AttackMonitoringTool/FoeAttackMonitoringTool/FoeAttackMonitoringTool/ImageWorker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoeAttackMonitoringTool
+{
+    public class ImageWorker
+    {
+        public static Bitmap CaptureActiveWindow()
+        {
+            // Shot size = screen size
+            Size shotSize = Screen.PrimaryScreen.Bounds.Size;
+
+            // the upper left point in the screen to start shot
+            // 0,0 to get the shot from upper left point
+            Point upperScreenPoint = new Point(0, 0);
+
+            // the upper left point in the image to put the shot
+            Point upperDestinationPoint = new Point(0, 0);
+
+            // create image to get the shot in it
+            Bitmap shot = new Bitmap(shotSize.Width, shotSize.Height);
+
+            // new Graphics instance 
+            Graphics graphics = Graphics.FromImage(shot);
+
+            // get the shot by Graphics class 
+            graphics.CopyFromScreen(upperScreenPoint, upperDestinationPoint, shotSize);
+
+            // return the image
+            return shot;
+        }
+
+        public static bool ImageContainsTemplate(Bitmap sourceImage, Bitmap template)
+        {
+            const Int32 divisor = 4;
+            const Int32 epsilon = 10;
+
+            ExhaustiveTemplateMatching etm = new ExhaustiveTemplateMatching(0.9f);
+
+            TemplateMatch[] tm = etm.ProcessImage(
+                new ResizeNearestNeighbor(template.Width / divisor, template.Height / divisor).Apply(template),
+                new ResizeNearestNeighbor(bmp.Width / divisor, bmp.Height / divisor).Apply(bmp)
+                );
+
+            if (tm.Length == 1)
+            {
+                Rectangle tempRect = tm[0].Rectangle;
+
+                if (Math.Abs(bmp.Width / divisor - tempRect.Width) < epsilon
+                    &&
+                    Math.Abs(bmp.Height / divisor - tempRect.Height) < epsilon)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ AttackMonitoringTool/FoeAttackMonitoringTool/FoeAttackMonitoringTool/MonitoringTool.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FoeAttackMonitoringTool
+{
+    partial class MonitoringTool
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(135, 55);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 0;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 55);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // MonitoringTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 124);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonStop);
+            this.Name = "MonitoringTool";
+            this.Text = "Monitoring Tool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
